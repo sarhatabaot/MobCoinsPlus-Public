3afea1a834f690dec36a113f6149bc83c9eefb5e
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-24 22:24:57 +0300

    Update CoinsTop.java

--- src/main/java/com/chup/mobcoinsplus/extras/CoinsTop.java
+++ src/main/java/com/chup/mobcoinsplus/extras/CoinsTop.java
@@ -1,19 +1,14 @@
 package com.chup.mobcoinsplus.extras;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
 public class CoinsTop {
-    public static HashMap<UUID, Integer> sortByValue(Map<UUID, Integer> hm)
-    {
-        List<Map.Entry<UUID, Integer> > list =
-                new LinkedList<Map.Entry<UUID, Integer> >(hm.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<UUID, Integer> >() {
-            public int compare(Map.Entry<UUID, Integer> o1, Map.Entry<UUID, Integer> o2)
-            {
-                return (o2.getValue()).compareTo(o1.getValue());
-            }
-        });
-        HashMap<UUID, Integer> temp = new LinkedHashMap<UUID, Integer>();
+    public static @NotNull Map<UUID, Integer> sortByValue(@NotNull Map<UUID, Integer> hm) {
+        List<Map.Entry<UUID, Integer> > list = new LinkedList<>(hm.entrySet());
+        list.sort((o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
+        HashMap<UUID, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<UUID, Integer> aa : list) { temp.put(aa.getKey(), aa.getValue()); }
         return temp;
     }
