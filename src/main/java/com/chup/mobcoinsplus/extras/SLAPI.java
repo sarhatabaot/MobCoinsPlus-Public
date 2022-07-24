90de30cfb6cde2c4d7a6bee20d32cbfeb0c92c1a
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-24 22:05:22 +0300

    Throwing dedicated exceptions instead of generic

--- src/main/java/com/chup/mobcoinsplus/extras/SLAPI.java
+++ src/main/java/com/chup/mobcoinsplus/extras/SLAPI.java
@@ -5,36 +5,38 @@ import org.bukkit.util.io.BukkitObjectOutputStream;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SLAPI {
-    public static void save(Object obj, String path) throws Exception {
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
-        oos.writeObject(obj);
-        oos.flush();
-        oos.close();
+    public static void save(Object obj, String path) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(path)))) {
+            oos.writeObject(obj);
+            oos.flush();
+        }
     }
 
-    public static Object load(String path) throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
-        Object result = ois.readObject();
-        ois.close();
-        return result;
+    public static Object load(String path) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(path)))) {
+            return ois.readObject();
+        }
     }
 
-    public static void bukkitSave(Object obj, String path) throws Exception {
-        BukkitObjectOutputStream oos = new BukkitObjectOutputStream(new FileOutputStream(path));
-        oos.writeObject(obj);
-        oos.flush();
-        oos.close();
+    public static void bukkitSave(Object obj, String path) throws IOException {
+        try (BukkitObjectOutputStream oos = new BukkitObjectOutputStream(Files.newOutputStream(Paths.get(path)))) {
+            oos.writeObject(obj);
+            oos.flush();
+        }
     }
 
-    public static Object bukkitLoad(String path) throws Exception {
-        BukkitObjectInputStream ois = new BukkitObjectInputStream(new FileInputStream(path));
-        Object result = ois.readObject();
-        ois.close();
-        return result;
+    public static Object bukkitLoad(String path) throws IOException, ClassNotFoundException {
+        try (BukkitObjectInputStream ois = new BukkitObjectInputStream(Files.newInputStream(Paths.get(path)))) {
+            return ois.readObject();
+        }
+
     }
 
 }
