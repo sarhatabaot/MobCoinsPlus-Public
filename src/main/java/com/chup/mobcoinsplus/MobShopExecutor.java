f46ca96b46896109620d2c00b7321b74cbb0626b
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-24 21:53:23 +0300

    Refactored MobShop Command

--- src/main/java/com/chup/mobcoinsplus/MobShopExecutor.java
+++ src/main/java/com/chup/mobcoinsplus/MobShopExecutor.java
@@ -6,34 +6,38 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class MobShopExecutor implements CommandExecutor {
 
     String prefix = ChatColor.translateAlternateColorCodes('&', Config.getPluginPrefix());
 
     private final Main plugin;
+
     public MobShopExecutor(Main plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-        if(sender instanceof Player) {
-            Player player = (Player) sender;
-            if(Config.getPermissionStatus()) {
-                if (player.hasPermission("mobcoinsplus.mobshop")) {
-                    new MobShopGUI(player, 1);
-                } else {
-                    String message = plugin.getMessages().getString("no-permission");
-                    player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&', message));
-                }
-            } else {
-                new MobShopGUI(player, 1);
-            }
-        } else {
-            System.out.println("This command can't be run through console.");
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            plugin.getLogger().warning("This command can't be run through console.");
+            return false;
         }
-        return false;
+
+        Player player = (Player) sender;
+        if (!Config.getPermissionStatus()) {
+            new MobShopGUI(player, 1);
+            return true;
+        }
+
+        if (player.hasPermission("mobcoinsplus.mobshop")) {
+            new MobShopGUI(player, 1);
+            return true;
+        }
+
+        String message = plugin.getMessages().getString("no-permission");
+        player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&', message));
+        return true;
     }
 }
