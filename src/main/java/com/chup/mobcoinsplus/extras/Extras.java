165340bbcb07179d17d3f1cdad6deca7448284ca
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-25 10:24:31 +0300

    Refactor for better redability

--- src/main/java/com/chup/mobcoinsplus/extras/Extras.java
+++ src/main/java/com/chup/mobcoinsplus/extras/Extras.java
@@ -7,48 +7,34 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 public class Extras {
 
-    static String prefix = ChatColor.translateAlternateColorCodes('&', Config.getPluginPrefix());
-
-    public static void giveCoins(Player player, int amount) {
+    public static void giveCoins(@NotNull Player player, int amount) {
         if (!Main.getPoints().containsKey(player.getUniqueId())) {
-
             Main.getPoints().put(player.getUniqueId(), amount);
-
         } else {
-
             int playerPoints = Main.getPoints().get(player.getUniqueId());
-
-            Main.getPoints().put(player.getUniqueId(), Main.getPoints().get(player.getUniqueId()) + amount);
-
+            Main.getPoints().put(player.getUniqueId(), playerPoints + amount);
         }
 
     }
 
-    public static void removeCoins(Player player, int amount) {
-
+    public static void removeCoins(@NotNull Player player, int amount) {
         if (!Main.getPoints().containsKey(player.getUniqueId())) {
-
             Main.getPoints().put(player.getUniqueId(), -amount);
-
         } else {
-
             int playerPoints = Main.getPoints().get(player.getUniqueId());
-
             Main.getPoints().put(player.getUniqueId(), playerPoints - amount);
-
         }
 
     }
 
-    public static void setCoins(Player player, int amount) {
-
+    public static void setCoins(@NotNull Player player, int amount) {
         Main.getPoints().put(player.getUniqueId(), amount);
-
     }
 
     public static Integer getCoins(UUID uuid) {
@@ -58,51 +44,49 @@ public class Extras {
         return Main.getPoints().get(uuid);
     }
 
-    public static ItemStack duplicateItem(Player player) {
-        if (player.getItemInHand() != null) {
-            ItemStack duplicateItem = new ItemStack(player.getItemInHand());
-            ItemMeta duplicateItemMeta = duplicateItem.getItemMeta();
-            duplicateItem.setItemMeta(duplicateItemMeta);
-            return duplicateItem;
-        }
-        return null;
+    public static @NotNull ItemStack duplicateItem(@NotNull Player player) {
+        return player.getInventory().getItemInMainHand().clone();
     }
 
-    public static ItemStack getColor(String color) {
-        if(color.equalsIgnoreCase("black")) {
-            return XMaterial.BLACK_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("red")) {
-            return XMaterial.RED_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("green")) {
-            return XMaterial.GREEN_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("brown")) {
-            return XMaterial.BROWN_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("blue")) {
-            return XMaterial.BLUE_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("purple")) {
-            return XMaterial.PURPLE_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("cyan")) {
-            return XMaterial.CYAN_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("light gray") || color.equalsIgnoreCase("light_gray") || color.equalsIgnoreCase("lightgray")) {
-            return XMaterial.LIGHT_GRAY_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("gray")) {
-            return XMaterial.GRAY_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("pink")) {
-            return XMaterial.PINK_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("lime")) {
-            return XMaterial.LIME_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("yellow")) {
-            return XMaterial.YELLOW_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("light blue") || color.equalsIgnoreCase("light_blue") || color.equalsIgnoreCase("lightblue")) {
-            return XMaterial.LIGHT_BLUE_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("magenta")) {
-            return XMaterial.MAGENTA_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("orange")) {
-            return XMaterial.ORANGE_STAINED_GLASS_PANE.parseItem();
-        } else if (color.equalsIgnoreCase("white")) {
-            return XMaterial.WHITE_STAINED_GLASS_PANE.parseItem();
+    public static ItemStack getColor(@NotNull String color) {
+        switch (color.toLowerCase()) {
+            case "black":
+                return XMaterial.BLACK_STAINED_GLASS_PANE.parseItem();
+            case "red":
+                return XMaterial.RED_STAINED_GLASS_PANE.parseItem();
+            case "green":
+                return XMaterial.GREEN_STAINED_GLASS_PANE.parseItem();
+            case "brown":
+                return XMaterial.BROWN_STAINED_GLASS_PANE.parseItem();
+            case "blue":
+                return XMaterial.BLUE_STAINED_GLASS_PANE.parseItem();
+            case "purple":
+                return XMaterial.PURPLE_STAINED_GLASS_PANE.parseItem();
+            case "light gray":
+            case "light_gray":
+            case "lightgray":
+                return XMaterial.LIGHT_GRAY_STAINED_GLASS_PANE.parseItem();
+            case "gray":
+                return XMaterial.GRAY_STAINED_GLASS_PANE.parseItem();
+            case "pink":
+                return XMaterial.PINK_STAINED_GLASS_PANE.parseItem();
+            case "lime":
+                return XMaterial.LIME_STAINED_GLASS_PANE.parseItem();
+            case "yellow":
+                return XMaterial.YELLOW_STAINED_GLASS_PANE.parseItem();
+            case "light blue":
+            case "light_blue":
+            case "lightblue":
+                return XMaterial.LIGHT_BLUE_STAINED_GLASS_PANE.parseItem();
+            case "magenta":
+                return XMaterial.MAGENTA_STAINED_GLASS_PANE.parseItem();
+            case "orange":
+                return XMaterial.ORANGE_STAINED_GLASS_PANE.parseItem();
+            case "white":
+                return XMaterial.WHITE_STAINED_GLASS_PANE.parseItem();
+            default:
+                return XMaterial.GLASS_PANE.parseItem();
         }
-        return XMaterial.GLASS_PANE.parseItem();
     }
 
 }
